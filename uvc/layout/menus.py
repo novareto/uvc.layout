bc728a998e0e0873f261c7841101eec6d24ae69d
trollfot <trollfot@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2010-04-27 17:19:50 +0000

    Added category sorting

--- uvc/layout/menus.py
+++ uvc/layout/menus.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import martian
 import grokcore.viewlet as grok
 from dolmen import menu
 from zope.component import queryMultiAdapter
 from zope.interface import Interface
 from uvc.layout import interfaces
+from megrok.pagetemplate import PageTemplate, view
 
 grok.templatedir('templates')
 grok.context(Interface)
+
+
+class category(martian.Directive):
+    scope = martian.CLASS
+    store = martian.ONCE
+    validate = martian.validateText
 
 
 class GlobalMenu(menu.Menu):
@@ -30,6 +38,31 @@ class SidebarMenu(menu.Menu):
     grok.name("uvc.user.sidebar")
     grok.title("Navigation")
     grok.implements(interfaces.ISidebar)
+
+    categories = None
+
+    def get_categories(self):
+        if self.categories is not None:
+            for name, items in self.categories.items():
+                yield {'title': name, 'entries': items}
+
+    def sort_by_keyword(self):
+        categories = {}
+        for viewlet in self.viewlets:
+            name = category.bind(default=u'').get(viewlet)
+            cat = categories.get(name)
+            if cat is None:
+                cat = categories[name] = []
+            cat.append(viewlet)
+        return categories
+
+    def update(self):
+        menu.Menu.update(self)
+        self.categories = self.sort_by_keyword()
+
+
+class Dropdowns(PageTemplate):
+    view(SidebarMenu)
 
 
 class HelpMenu(menu.Menu):
