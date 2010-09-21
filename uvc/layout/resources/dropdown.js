a0b731c761b6101624518a59cc4c1d3865762411
trollfot <trollfot@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2010-09-21 16:03:17 +0000

    Corrected dropdown with pure CSS. Added timer options, if further problems happen.

--- uvc/layout/resources/dropdown.js
+++ uvc/layout/resources/dropdown.js
@@ -1,12 +1,31 @@
+var timeout    = 0;
+var closetimer = 0;
+var opendd     = null;
+
+function dd_close() { 
+   opendd.hide();
+}
+
+function dd_timer() {
+  closetimer = window.setTimeout(dd_close, timeout);
+}
+
+function dd_canceltimer() {
+  if(closetimer) {
+     window.clearTimeout(closetimer);
+     closetimer = null;
+  }
+}
+
 $(document).ready(function(){
     $("dl.dropdown").hover(
         function() {
-           $("dd", this).show();
-           $(this).addClass("unfolding");
+           dd_canceltimer();
+	   if (opendd != null) { dd_close(); }
+           opendd = $("dd", this).show();
         }, 
         function() {
-           $("dd", this).hide();
-           $(this).removeClass("unfolding");
+	   dd_timer();
         } 
     );
 });
