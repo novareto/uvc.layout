eea58877b0321a10e04665fba661be7e93609f52
trollfot <trollfot@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2013-11-08 14:38:15 +0000

    Using now uvc.layout and publishing Page and TablePage with the GrokView methods

--- uvc/layout/__init__.py
+++ uvc/layout/__init__.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2007-2011 NovaReto GmbH
-# cklinger@novareto.de 
+# cklinger@novareto.de
 
+import grokcore.message
+from grok import baseclass, util as grok_util
+from grok.interfaces import IGrokView
+from grokcore.layout import Page as BasePage
+from megrok.z3ctable import TablePage
 
 from uvc.layout.interfaces import *
 from uvc.layout.slots.components import Menu, MenuItem, SubMenu
 from uvc.layout.forms.event import IAfterSaveEvent
+
+from zope.interface import implementer
+
+
+@implementer(IGrokView)
+class GrokView(object):
+    
+    def application_url(self, name=None, data=None):
+        return grok_util.application_url(self.request, self.context, name, data)
+
+    def flash(self, message, type='message'):
+        grokcore.message.send(message, type=type, name='session')
+
+
+class Page(GrokView, BasePage):
+    baseclass()
+
+
+class TablePage(GrokView, TablePage):
+    baseclass()
